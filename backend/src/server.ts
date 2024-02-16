f7e4dbead53a3d634c59d73d98e4143b40439c2f
Amkam <amaduskamara@gmail.com>
2024-02-16 00:00:16 +0000

    Connect to db with mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
