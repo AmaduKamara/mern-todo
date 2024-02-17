720aa8653f25d7049558005321778f62bf69a864
Amkam <amaduskamara@gmail.com>
2024-02-17 04:05:36 +0000

    Add formated date function

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -1,6 +1,7 @@
 import { Card, Badge } from "react-bootstrap";
 import { Todo as TodoModel } from "../models/todo";
-import styles from '../styles/Todo.module.css'
+import styles from "../styles/Todo.module.css";
+import { formatDate } from "../utils/formatDate";
 
 interface TodoProps {
   todo: TodoModel;
@@ -9,16 +10,25 @@ interface TodoProps {
 
 const Todo = ({ todo, className }: TodoProps) => {
   const { title, text, priority, createdAt, updatedAt } = todo;
+
+  let createdUpdatedText: string;
+
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  }
+
   return (
     <Card className={`${styles.todoCard} ${className}`}>
       <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
-        <Badge bg={`${priority === 'low' ? "warning" : "danger"}`}>{priority}</Badge>
+        <Badge bg={`${priority === "low" ? "warning" : "danger"}`}>
+          {priority}
+        </Badge>
       </Card.Body>
-      <Card.Footer className="text-muted">
-        {createdAt}
-      </Card.Footer>
+      <Card.Footer className='text-muted'>{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
